7cb8c08f8ec16c4814300c32321dad89fab2a970
erickseis <erickseis@gmail.com>
2022-07-17 10:12:28 -0400

    sad

--- src/Location.jsx
+++ src/Location.jsx
@@ -21,8 +21,20 @@ const Location = () => {
     }
     // location.residents
 
-
     console.log(location)
+    const [page, setPage] = useState(1)
+    const lastIndex = page * 5
+    const firstIndex = lastIndex - 5
+    const residentsPaginated = location.residents?.slice(firstIndex, lastIndex)
+
+    const lastPage = Math.ceil(location.residents?.length / 5)
+    console.log(lastPage)
+    const numbers = []
+    
+    for(let i = 1 ; i<= lastPage; i++){
+        numbers.push(i)
+    }
+
     return (
 
         <div className='contenedor-padre'>
@@ -34,24 +46,37 @@ const Location = () => {
             </div>
             <div className='contenedor-hijo'>
                 <br />
-                <br /> 
+                <br />
                 <div className='barra-lista'>
-                <h1 className='name'> <strong>{location.name}</strong></h1>
-                <ul className='ul-main'>
-                
-                    <li> <strong> Type: <br />  {location.type} </strong></li>
-                    <li> <strong> Dimension: <br />  {location.dimension}</strong></li>
-                    <li> <strong> Episodes: <br />  {
-                        location.residents?.length}</strong></li>
-                </ul>
+                    <h1 className='name'> <strong>{location.name}</strong></h1>
+                    <ul className='ul-main'>
+
+                        <li> <strong> Type: <br />  {location.type} </strong></li>
+                        <li> <strong> Dimension: <br />  {location.dimension}</strong></li>
+                        <li> <strong> Episodes: <br />  {
+                            location.residents?.length}</strong></li>
+                    </ul>
 
                 </div>
                 <div className='cards'>
-                    {location.residents?.map(character => (
+                    {residentsPaginated?.map(character => (
 
                         <ResidentInfo character={character} key={character} />
                     ))}
                 </div>
+            </div>
+            <br />
+            <br />
+            <div className='Btn-container'>
+            <button className='Btn-Prev' onClick={() => setPage(page - 1)}
+                disabled={page === 1}
+            >Prev Page</button>
+            {numbers.map(number=> (
+                <button className='Btn-Numbers' onClick={() => setPage(number)} >{number}</button>
+             ))}
+            <button className='Btn-Next' onClick={() => setPage(page + 1)}
+            disabled={page ===lastPage}
+            >Next Page</button>
             </div>
         </div>
     );
